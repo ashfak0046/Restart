c4de8a3992a5f1dcd7b4949d602a941e54f47a7d
ashfak0046 <ashfakmahbub006@gmail.com>
2024-01-23 23:33:54 +0600

    h

--- Data_Type_Conversion/comparision.js
+++
@@ -1,8 +0,0 @@
-console.log(2 > 1);
-console.log("2" > 2);
-console.log("2" == 2)
-console.log(1 == true)
-console.log(1 === true)
-console.log("first" == true)
-console.log(0 ==  undefined)
-console.log(0 == null) 
