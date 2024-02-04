9511d614deb552ee738bcaa6d202655b5396c8a0
ashfak0046 <ashfakmahbub006@gmail.com>
2024-02-04 12:36:03 +0600

    function-3

--- 12_function_part_1.js
+++ 12_function_part_1.js
@@ -11,7 +11,7 @@
 // }
 
 // const result = addTwoNumbers(1, 2);
-// console.log(result)
+// console.log(result) 
 // addTwoNumbers(2,3);
 // addTwoNumbers("2",4);
 // addTwoNumbers(3,"5");
@@ -19,12 +19,42 @@
 // addTwoNumbers(12,null);
 // addTwoNumbers(3);
 
-function loginUser(username = "Mahi"){
-    if(!username){
-        console.log("Please enter a username");
-        return;
-    }
-    return `${username} just logged in`;
+// function loginUser(username = "Mahi"){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return;
+//     }
+//     return `${username} just logged in`;
+// }
+
+// console.log(loginUser())  
+
+
+/**
+ * ?Rest Operator 
+ */
+// function CalculateUserCart(...num1){
+//     return num1;
+// }
+
+// console.log(CalculateUserCart(2,5,6,3));
+
+function CalculateUserCart(val1,val2,...num1){
+    console.log(val1,val2);
+    return val1,val2, num1;
 }
 
-console.log(loginUser())
+console.log(CalculateUserCart(2,5,6,3));
+
+// *Fetching values from Object using Function 
+
+const user ={
+    userName: "Ashfak",
+    age: 25,
+}
+
+function handleObject(obj) {
+    console.log(`My name is ${obj.userName} and age is ${obj.age}`);
+}
+
+handleObject(user);
