f96b4cafcc858d95d1ece244d64ade33d16b5615
ashfak0046 <ashfakmahbub006@gmail.com>
2024-02-04 14:26:03 +0600

    s

--- 13_scope.js
+++ 13_scope.js
@@ -1,13 +1,25 @@
-var a = 10;
-let b = 20; // *global scope
-let c = 30;
+// var a = 10;
+// let b = 20; // *global scope
+// let c = 30;
 
-if(true){
-    let b = 50; // ?block scope
-    let d = 100;
-    console.log(a); 
-    console.log(b);
-    console.log(c);
+// if(true){
+//     let b = 50; // ?block scope
+//     let d = 100;
+//     console.log(a); 
+//     console.log(b);
+//     console.log(c);
+// }
+
+// // console.log(d)
+
+var x = 40;
+
+function createFunction1(){
+    const x = 30;
+    function nestedFunction (){
+        console.log(x)
+    }
+    nestedFunction();
 }
 
-// console.log(d)
+createFunction1();
