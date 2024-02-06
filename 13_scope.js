ad8491e9cf0230b68cebc286581e1cbee3558315
ashfak0046 <ashfakmahbub006@gmail.com>
2024-02-06 12:26:52 +0600

    scope

--- 13_scope.js
+++ 13_scope.js
@@ -5,21 +5,59 @@
 // if(true){
 //     let b = 50; // ?block scope
 //     let d = 100;
-//     console.log(a); 
+//     console.log(a);
 //     console.log(b);
 //     console.log(c);
 // }
 
 // // console.log(d)
 
-var x = 40;
+// var x = 40;
 
-function createFunction1(){
-    const x = 30;
-    function nestedFunction (){
-        console.log(x)
-    }
-    nestedFunction();
+// function createFunction1(){
+//     const x = 30;
+//     function nestedFunction (){
+//         console.log(x)
+//     }
+//     nestedFunction();
+// }
+
+// createFunction1();
+
+// function one(){
+//     const username = "Ashfak";
+
+//     function two(){
+//         const website = "youtube.com";
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two();
+// }
+
+//  one();
+
+//  if(true){
+//     const username = "Mahbub"
+
+//     if(username === "Mahbub"){
+//         const website = "https://website.com";
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+//  }
+
+//  console.log(username)
+
+console.log(addNumbers(3));
+console.log(addNumbers2(6));
+
+
+function addNumbers(num) {
+  return num + 1;
 }
 
-createFunction1();
+const addNumbers2 = function (num) {
+  //? function which is stored in a variable is not possible before initialization
+  return num + 2;
+};
