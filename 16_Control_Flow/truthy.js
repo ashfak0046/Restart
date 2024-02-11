ca853a188ef8bc465f7bf82178602c36eb0bf67e
ashfak0046 <ashfakmahbub006@gmail.com>
2024-02-12 00:01:45 +0600

    control-flow

--- 16_Control_Flow/truthy.js
+++ 16_Control_Flow/truthy.js
@@ -1,17 +1,34 @@
-const userEmail  = [];
+const userEmail = [];
 
-if(userEmail){
-    console.log("Email found");
-}
-else{
-    console.log("Enter your email address");
+if (userEmail) {
+  console.log("Email found");
+} else {
+  console.log("Enter your email address");
 }
 
 // * Falsy value -> false, 0, -0, BigInt On, "", null, undefined, NaN
-// ? Truthy value -> "0", 'false', " ", [],{}, function(){}, 
+// ? Truthy value -> "0", 'false', " ", [],{}, function(){},
 
 const emptyObj = {};
 
-if(Object.keys(emptyObj).length ===  0){
-    console.log("Empty object")
+if (Object.keys(emptyObj).length === 0) {
+  console.log("Empty object");
 }
+
+
+//? Nullish Coalescing Operator -> ??: null & undefined
+
+// let value = undefined;
+// // value = value ?? 15;
+// // value = null ?? "" ?? 3;
+// // value = null ?? {} ?? 3;
+// // value = null ?? function(){} ?? 3;
+// // value = null ?? false ?? 3;
+// value = null ?? 0 ?? 3;
+// console.log(value)
+
+//! Terniary Operators: condition ? true : false
+
+const watchPrice = 400;
+
+watchPrice >= 300 ? console.log("Greater than 300") : console.log("Less than 300");
